52e3c025c3a9bb335383fcb7c7b5a9efb396e8b2
mengli <1756935704@qq.com>
2021-05-14 23:28:19 +0800

    Update my.sh

--- my.sh
+++ my.sh
@@ -17,8 +17,7 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git adgurd https://github.com/liuran001/openwrt-packages/tree/packages/luci-app-adguardhome' feeds.conf.default
 sed -i '$a src-git smart https://github.com/liuran001/openwrt-packages/tree/packages/smartdns' feeds.conf.default
-sed -i '$a src-git appsmart https://github.com/liuran001/openwrt-packages/tree/packages/luci-app-smartdns
-' feeds.conf.default
+sed -i '$a src-git appsmart https://github.com/liuran001/openwrt-packages/tree/packages/luci-app-smartdns' feeds.conf.default
 sed -i '$a src-git apppass https://github.com/menglihuan/openwrt-packages/tree/master/luci-app-passwall' feeds.conf.default
 sed -i '$a src-git small https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
