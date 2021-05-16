c2ca37ba4ef6a3c81cea188eb8396919c8838e2b
mengli <1756935704@qq.com>
2021-05-16 11:24:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git menglihuan https://github.com/menglihuan/openwrt' feeds.conf.default
+sed -i '$a src-git hello https://github.com/fw876/helloworld' feeds.conf.default
+
+
 
